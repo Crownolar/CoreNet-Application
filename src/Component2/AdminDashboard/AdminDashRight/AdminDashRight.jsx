b967ae4bba24171a233a7ee7c8375ba8592aeaed
Oriade, Yusuf Olamilekan <crownhorla07@gmail.com>
2023-08-03 20:39:46 +0100

    sixth push

--- src/Component2/AdminDashboard/AdminDashRight/AdminDashRight.jsx
+++ src/Component2/AdminDashboard/AdminDashRight/AdminDashRight.jsx
@@ -8,6 +8,7 @@ import { BiTask } from "react-icons/bi";
 import { GoTasklist } from "react-icons/go";
 import { MdAddTask } from "react-icons/md";
 import { BsListTask } from "react-icons/bs";
+import { PiSignOut } from "react-icons/pi";
 import AdminDashCreateWriter from "./AdminDashCreateWriter/AdminDashCreateWriter";
 import AdminAllWriter from "./AdminAllWriter/AdminAllWriter";
 import AdminDashCreateWriterNextContent from "./AdminDashCreateWriter/AdminDashCreateWriterNextContent/AdminDashCreateWriterNextContent";
@@ -19,7 +20,7 @@ const AdminDashRight = () => {
 
     openSiderBar && (
      <div className="Adminsidebar_MobileView">
-       <div className="AdminSide_Wrap">
+       <div className=".AdminSide_Wrap">
          <div className="Adminsidebarlogo">
            <img src="/CORENETrem.png" alt="" className="AdminLogo_SideBar" />
          </div>
@@ -40,17 +41,18 @@ const AdminDashRight = () => {
                <GoTasklist />
                <p>Task Assignment</p>
              </div>
-             <div className="Admintask">
+             <div className="Admintask" onClick={() => navigate("./adminpage/admincreatewriter")}>
                <MdAddTask />
                <p> Create writer</p>
              </div>
-             <div className="Admintask">
+             <div className="Admintask" onClick={()=> navigate("/adminpage/adminallwriter")}>
                <BsListTask />
                <p>All writers</p>
              </div>
            </div>
-           <div className="AdminSignOutDiv">
-             <p onClick={() => navigate("../Login")}>Sign Out</p>
+           <div className="AdminSignOutDiv" onClick={() => navigate("../signout")}>
+           <PiSignOut />
+             <p>Sign Out</p>
            </div>
          </div>
        </div>
