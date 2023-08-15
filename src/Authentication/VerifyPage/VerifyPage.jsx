17836a2ce610911412c58edfb68e1072fee952c0
Oriade, Yusuf Olamilekan <crownhorla07@gmail.com>
2023-08-15 09:36:19 +0100

    Thirty seventh push

--- src/Authentication/VerifyPage/VerifyPage.jsx
+++ src/Authentication/VerifyPage/VerifyPage.jsx
@@ -1,27 +1,52 @@
 import React, { useEffect, useState } from "react";
 import "./VerifyPage.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const VerifyPage = () => {
-  const Nav = useNavigate()
+  const { token } = useParams();
+    const [isVerified, setIsVerified] = useState(1)
+    console.log(token)
+    const Nav = useNavigate()
+
+    useEffect(() => {
+      const verifyUser = () => {
+          axios
+              .post(`https://corenet-api.onrender.com/api/verify-email/${token}`)
+              .then((res) => {
+                  console.log(res);
+                  setIsVerified(2)
+              })
+              .catch((error) => {
+                console.error("Error:", error);
+                if (error.response) {
+                  console.error("Response Data:", error.response.data);
+                }
+              });
+      };
+      verifyUser()
+  }, []);
+
+
+
+  
   return (
     <div className="veifypage">
       <div className="verifypagewrap">
         <div className="verificationpageaction">
-          <h3>Verification successful</h3>
+         {isVerified === 1 ?<>  <h3>Verification successful</h3>
 
-          <>
-            <p>Congratulations!</p>
-            <p style={{textAlign: "center"}}>
-              Your account has been successfully verified. You can now enjoy
-              full access to our services and features.
-            </p>
-          </>
+<>
+  <p>Congratulations!</p>
+  <p style={{textAlign: "center"}}>
+    Your account has been successfully verified. 
+  </p>
+</>
 
-          <p>Click ok to continue</p>
+<p>Click ok to continue</p></> : "Verification Failed"}
         </div>
         <div className="verificationcontinue">
-          <button onClick={() => Nav("/")}>Ok</button>
+          <button onClick={() => Nav("./login")}>{isVerified === 1 ? "Sign in" : "Resend Email"}</button>
         </div>
       </div>
     </div>
